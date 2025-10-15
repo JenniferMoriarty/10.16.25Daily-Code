f305f1404c96568e583c8aa19223df643d4e7895
Jen Moriarty <JMoriarty78@gmail.com>
2025-10-15 11:12:36 -0600

    Modify loop to print numbers 4-16 by 2s
    Updated the for loop to print numbers from 4 to 16, counting by 2s.

---
+++ for loop.cpp
@@ -0,0 +1,11 @@
+//instructions: change this loop so it prints out the numbers 4-16 counting by 2s
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i <= 5; i++) { //remember "i++" can be written as "i+=1"
+        cout << i << " ";
+    } //technically the {} aren't needed unless you have more than one line, but it's good coding practice to include them!
+
+}
