aeb0694056d66afc583f26801052d600a0d73d67
Jen Moriarty <JMoriarty78@gmail.com>
2025-10-15 11:14:07 -0600

    Update while loop to count down from 80 to 20
    Modify the while loop to count down from 80 to 20 by 10s.

---
+++ while loop.cpp
@@ -0,0 +1,14 @@
+//instructions: modify this loop so it starts at 80 and counts down to 20 by 10s
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 1; //starting point
+
+    while (x <= 5) {
+        cout << "x = " << x << endl; //the "x = " is optional :)
+        x++;
+    }
+
+}
